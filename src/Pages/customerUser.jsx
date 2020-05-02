0fd4bc06fc1161e1a9e73fb3bae85c3af1a915b8
lsn <lsn@one2team.com>
2020-05-02 22:07:41 +0200

    from lsn :)

--- src/Pages/customerUser.jsx
+++ src/Pages/customerUser.jsx
@@ -1,4 +1,5 @@
 import React, {Component}from "react";
+import { Table } from 'antd';
 import logo from "../logo.svg";
 
 
@@ -6,36 +7,52 @@ class customerUserPage extends Component{
     constructor(props){
         super(props);
         this.state={
-            customers:[]
+            customers: null,
+            showPersonalInfo: false,
+            selectedId: null,
         }
     }
 
     callAPI(){
-            fetch("http://localhost:8000/customers")
-            .then(res =>res.text())
+            fetch("http://localhost:8000/customers", {
+                headers: {
+                'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
             .then((res) => this.setState({customers: res}));
     }
 
-    componentWillMount() {
-        this.callAPI();
+    callAPI2(){
+        fetch(`http://localhost:8000/customers/${this.state.selectedId}`, {
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        })
     }
 
+    componentWillMount() {
+       this.callAPI();
+    }
+ 
+    handleInputChange = (event) => {
+        this.setState({selectedId: event.target.value});
+    } 
 
     homeClick = () => {
         this.props.history.push("/");
     };
 
     personInfoClick = () => {
-
-
+        this.setState({showPersonalInfo: true});
     };
 
     updateClick = () => {
-
+        this.setState({showPersonalInfo: false});
     };
 
     itemIdClick = () => {
-
+        this.callAPI2()
     };
 
     billNoClick = () => {
@@ -43,6 +60,47 @@ class customerUserPage extends Component{
     };
 
     render(){
+        console.log(this.state.customers)
+        const dataSource = this.state.customers? this.state.customers: [];
+          
+          const columns = [
+            {
+              title: 'address',
+              dataIndex: 'address',
+              key: 'address',
+            },
+            {
+              title: 'number',
+              dataIndex: 'contact_number',
+              key: 'contact_number',
+            },
+            {
+              title: 'email',
+              dataIndex: 'customer_email',
+              key: 'customer_email',
+            },
+            {
+                title: 'customer_id',
+                dataIndex: 'customer_id',
+                key: 'customer_id',
+            },
+            {
+                title: 'customer_name',
+                dataIndex: 'customer_name',
+                key: 'customer_name',
+            },
+            {
+                title: 'prefer_store_id',
+                dataIndex: 'prefer_store_id',
+                key: 'prefer_store_id',
+            },
+            {
+                title: 'total_orders',
+                dataIndex: 'total_orders',
+                key: 'total_orders',
+            },
+          ];
+          
         return (
             <div className="App">
                 <header className="App-header">
@@ -60,7 +118,7 @@ class customerUserPage extends Component{
                     <div>
                         <label>Item_ID:</label>
                         <div>
-                            <input type="Item_ID" className="form-control" id="Item_ID" placeholder="Enter Item_ID.." name="Item_ID"/>
+                            <input type="Item_ID" onChange={this.handleInputChange} className="form-control" id="Item_ID" placeholder="Enter Item_ID.." name="Item_ID"/>
                         </div>
                     </div>
                 </form>
@@ -76,9 +134,13 @@ class customerUserPage extends Component{
                 </form>
                 <button onClick={this.billNoClick}>search</button>
 
-                <h1>
-                    {'I wanna show a result table here'}
-                </h1>
+                <div>
+                    <Table dataSource={dataSource} columns={columns} />
+                </div>
+            
+                {/* <h1>
+                    { this.state.showPersonalInfo && this.state.customers[0] && this.state.customers }
+                </h1> */}
             </div>
         )
     }
--- src/server.js
+++ src/server.js
@@ -7,61 +7,68 @@ const app = express();
 const database = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'localhost',
         user : 'postgres',
-        password : 'Mm13580373527',
-        database : 'retail_db'
+        password : 'shenlin',
+        database : 'retailer_db'
     }
 });
 
 
 app.use(cors());
 app.use(bodyParser.json());
-database.select('*').from('customers').then(data=>{
-    app.get('/customers', (req, res)=>{
+
+app.get('/test', (req, res) => res.send('Hello World!'))
+
+app.get('/customers', (req, res)=>{
+    console.log('in customers')
+    database.select('*').from('customers').then(data=>{
+        res.setHeader('Content-Type', 'application/json');
         res.send(data)
-    })
-});
+    });  
+    consle.log('hello')
+})
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-database.select('*').from('inventory').then(data=>{
-    app.get('/inventory', (req, res)=>{
-        res.send(JSON.stringify(data))
-    })
-});
+// database.select('*').from('inventory').then(data=>{
+//     app.get('/inventory', (req, res)=>{
+//         res.send(JSON.stringify(data))
+//     })
+// });
 
-database.select('*').from('items').then(data=>{
-    app.get('/items', (req, res)=>{
-        res.send(JSON.stringify(data))
-    })
-});
+// database.select('*').from('items').then(data=>{
+//     app.get('/items', (req, res)=>{
+//         res.send(JSON.stringify(data))
+//     })
+// });
 
-database.select('*').from('items_sold').then(data=>{
-    app.get('/items_sold', (req, res)=>{
-        res.send(JSON.stringify(data))
-    })
-});
+// database.select('*').from('items_sold').then(data=>{
+//     app.get('/items_sold', (req, res)=>{
+//         res.send(JSON.stringify(data))
+//     })
+// });
 
-database.select('*').from('purchases').then(data=>{
-    app.get('/purchases', (req, res)=>{
-        res.send(JSON.stringify(data))
-    })
-});
+// database.select('*').from('purchases').then(data=>{
+//     app.get('/purchases', (req, res)=>{
+//         res.send(JSON.stringify(data))
+//     })
+// });
 
-database.select('*').from('stores').then(data=>{
-    app.get('/stores', (req, res)=>{
-        res.send(JSON.stringify(data))
-    })
-});
+// database.select('*').from('stores').then(data=>{
+//     app.get('/stores', (req, res)=>{
+//         res.send(JSON.stringify(data))
+//     })
+// });
 
-database.select('*').from('suppliers').then(data=>{
-    app.get('/suppliers', (req, res)=>{
-        res.send(JSON.stringify(data))
-    })
-});
+// database.select('*').from('suppliers').then(data=>{
+//     app.get('/suppliers', (req, res)=>{
+//         res.send(JSON.stringify(data))
+//     })
+// });
 
 
 app.listen(8000,()=>{
